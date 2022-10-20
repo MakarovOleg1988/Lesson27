f7c7df71568f82ca4f1eb317d0937ed7ad27ffbe
MakarovOleg1988 <makarov.oleg1988@gmail.com>
2022-10-20 14:20:31 +0530

    Добавьте файлы проекта.

---
+++ Lesson27/Interfaces.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson27
+{
+    public interface IAudio // Передачи зависимости через интерфейс
+    {
+        void UpdateState()
+        {
+        }
+    }
+
+    public interface IGameManager
+    {
+        void OnEndUpdateState()
+        {
+
+        }
+    }
+}
---
+++ Lesson27/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Lesson27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public class ReferenceManager
+        {
+            public static GameManager _GM; // Передачи зависимости через статичный тип
+            public static GameAudiomanager _gameAudioManager; // Передачи зависимости через статичный тип
+        }
+
+        public class GameManager : IAudio
+        {
+
+            protected IAudio _audio;
+            protected IGameManager _GameManager;
+
+            public void UpdateState()
+            {
+                _GameManager.OnEndUpdateState();
+            }
+        }
+
+        public class GameAudiomanager: IGameManager
+        {
+            protected IGameManager _GameManager;
+            protected GameManager _GM;
+
+            public GameAudiomanager(IGameManager _GameManager, GameManager _GM) // Инъекция зависимости
+            {
+                _GM = _GM;
+                _GameManager = _GameManager;
+            }
+
+            void UpdateState()
+            {
+                ReferenceManager._GM.UpdateState();
+            }
+        }
+    }
+}
